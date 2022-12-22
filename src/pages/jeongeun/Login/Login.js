602af766ec60ffea6e770ac8971204fda9b219b4
jeong-eun-park <p.j.eun555@gmail.com>
2022-12-22 15:45:37 +0900

    ADD: first upload -Login -Main

--- src/pages/jeongeun/Login/Login.js
+++ src/pages/jeongeun/Login/Login.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import Style from './Login.scss';
 
 export default function LoginJ() {
-  return <></>;
+  return (
+    <div className="loginbox">
+      <div className="content">
+        <a href="#" className="logo">
+          Westagram
+        </a>
+
+        <div className="login">
+          <input
+            id="inputId"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input id="inputPw" type="text" placeholder="비밀번호" />
+          <button id="btn" type="button">
+            로그인
+          </button>
+        </div>
+
+        <div className="forgetpw">
+          <a href="#">비밀번호를 잊으셨나요?</a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/jeongeun/Main/Main.js
+++ src/pages/jeongeun/Main/Main.js
@@ -1,5 +1,67 @@
 import React from 'react';
+import Style from './Main.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCompass, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function MainJ() {
-  return <></>;
+  return (
+    <div className="content">
+      <nav>
+        <div className="instaicon">
+          <div className="logo">
+            <FontAwesomeIcon icon="fa-brands fa-instagram" />
+          </div>
+          <a href="#">Westagram</a>
+        </div>
+        <input className="search" type="text" placeholder="검색" />
+        <div className="icon">
+          <div className="icon1">
+            <FontAwesomeIcon icon="fa-regular fa-compass" />
+          </div>
+          <div className="icon2">
+            <FontAwesomeIcon icon="fa-regular fa-heart" />
+          </div>
+          <div className="icon3">
+            <FontAwesomeIcon icon="fa-regular fa-user" />
+          </div>
+        </div>
+      </nav>
+
+      <main>
+        <div className="feeds">
+          <article>
+            <img src="/images/jeongeun/westa.jpg" alt="feed photo" />
+            <div className="likeBox">
+              <img className="like" src="/images/jeongeun/heart.png" />
+              <img className="speech" src="/images/jeongeun/speech.png" />
+            </div>
+            <div className="commentbox">
+              <ul className="allcomment"></ul>
+            </div>
+            <div className="inputcomment">
+              <input
+                className="comment"
+                type="text"
+                placeholder="댓글을 입력하세요"
+              />
+              <button className="btn" type="button">
+                게시
+              </button>
+            </div>
+          </article>
+        </div>
+        <div className="main-right">
+          <div className="userProfile">
+            <img src="/images/jeongeun/cat.jpg" />
+            <span> username</span>
+            <button className="followButton" type="button">
+              팔로우
+            </button>
+          </div>
+          <div className="userProfile2"></div>
+        </div>
+      </main>
+    </div>
+  );
 }
