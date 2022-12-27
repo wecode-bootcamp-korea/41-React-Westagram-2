3f439ff7892abe271a05271099680f7e3e5a738c
Ha-Pyeongan <96smilepeace@gmail.com>
2022-12-27 21:51:15 +0900

    댓글 삭제, 좋아요 버튼 구현

--- src/pages/pyeongan/Main/components/MainAddComment.jsx
+++ src/pages/pyeongan/Main/components/MainAddComment.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
+import { React, useState, useEffect } from 'react';
 
-export default function MainAddComment(props) {
+export default function MainAddComment({ item }) {
+  const [bln, setBln] = useState(true);
+  const [heartColor, setHeartColor] = useState('black');
+  const onHeart = e => {
+    setBln(!bln);
+    console.log(bln);
+    return bln ? setHeartColor('red') : setHeartColor('black');
+  };
+
+  const divKiller = e => {
+    const divkill = document.getElementById(item.toString());
+    divkill.remove();
+  };
   return (
-    <div className="addcomment">
-      <span>hapyeongan : </span>
-      <span>{props.item}</span>
+    <div className="addcomment" id={item.toString()} key={item.toString()}>
+      <p>
+        {`userName : ${item}`}
+        <button onClick={divKiller} className="xBox">
+          <i class="fa-solid fa-x"></i>
+        </button>
+        <button onClick={onHeart} className="heart">
+          <i style={{ color: heartColor }} class="fa-solid fa-heart"></i>
+        </button>
+      </p>
     </div>
   );
 }
